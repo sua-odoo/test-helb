6507f3676ed73427c5071f6c623de8c464dcb9a7
Adrien Suys <sua@odoo.com>
2022-11-22 17:25:58 +0100

    add base module

---
+++ helb_project_base/__manifest__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "helb_project",
+    'summary': """Project for HELB Student""",
+    'author': "My Company",
+    'website': "http://www.yourcompany.com",
+    'category': 'Uncategorized',
+    'version': '15.0.0.1',
+    'depends': ['base', 'sale_management', 'calendar', 'hr'],
+    'data': [
+    ]
+}
